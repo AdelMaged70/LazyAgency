00edb90f896d83ce98a34765d8405875ed1d1a04
AdelMaged70 <adelmaged417@gmail.com>
2025-10-29 22:32:36 +0300

    add domain

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -35,7 +35,7 @@ export default function Contact() {
     setErrorMessage('')
 
     try {
-      const response = await fetch('http://localhost:3000/api/contact', {
+      const response = await fetch('lazy-agency.vercel.app/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
